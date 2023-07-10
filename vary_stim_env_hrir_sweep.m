f7edd257cd5e1258bd5c8969594344d1c34d1a25
Mark Saddler <msaddler@mit.edu>
2023-07-09 21:24:30 -0400

    simplifying francl code for generating binaural room impulse responses

--- vary_stim_env_hrir_sweep.m
+++ vary_stim_env_hrir_sweep.m
@@ -22,15 +22,6 @@ function [] = vary_stim_hrir_sweep(wall_xdim_str, wall_ydim_str, wall_zdim_str, 
     floor_material = str2num(floor_material_str);
     cieling_material = str2num(cieling_material_str);
     wtypes = [wall_material, wall_material, wall_material, wall_material, floor_material, cieling_material]; % [x=0 x=L y=0 y=W z=0 z=H]
-    %          8     Heavyweight drapery (walls)
-    % freqtable (Hz)
-    %           125     250     500     1000     2000    4000
-    % absorption
-    %			0.14	0.35	0.55	0.72     0.70    0.65
-    %          15    Carpet on foam rubber padding (floor)
-    %			0.08	0.24	0.57	0.69	0.71	0.73
-    %          18    Acoustic tiles, 0.5", 16" below ceiling
-    %			0.52	0.37	0.50	0.69	0.79	0.78
 
     f_samp_Hz = 44100;
     num_taps = 22050;
@@ -39,13 +30,6 @@ function [] = vary_stim_hrir_sweep(wall_xdim_str, wall_ydim_str, wall_zdim_str, 
     log_dist = 0;
     highpass = 1;
 
-    % fs = f_samp_Hz;
-    % c = f_samp_Hz;
-    % taps = num_taps;
-    % dspy = dsply;
-    % jittr = jitter;
-    % l_dist = log_dist;
-    % L = length(meas_locs(:,1));
     new_dir = sprintf('./Expanded_HRIRdist%d-5deg_elev_az_room%dx%dy%dz_materials%dwall%dfloor%dciel', round(speaker_dist * 100), wall_xdim, wall_ydim, wall_zdim, wall_material, floor_material, cieling_material);
 
     if ~exist(new_dir, 'dir')
@@ -79,24 +63,8 @@ function [] = vary_stim_hrir_sweep(wall_xdim_str, wall_ydim_str, wall_zdim_str, 
                 wtypes, f_samp_Hz, c_snd, num_taps, log_dist, jitter, highpass, dsply);
             toc
 
-            %t = (0:(1/f_samp_Hz):((num_taps - 1)/f_samp_Hz))';
-            %figure; plot(t, log(abs(h_out(:,1))))
-            %hold on; plot(t, log(abs(h_out(:,2))),'r')
-            %title('Wall Draperies, Padded Carpet Floor, Acoustic Ceiling Tiles')
-            %xlabel('time (s)')
-            %
-            %stim1 = audioread('stim268_piano.wav')
-            %stim1 = stim*10^-.5
             audiowrite(out_r, h_out(:, 2), 44100, 'BitsPerSample', 32);
             audiowrite(out_l, h_out(:, 1), 44100, 'BitsPerSample', 32);
-            %[l_hrir rate] = audioread('h_out_l_testpar.wav');
-            %[r_hrir rate] = audioread('h_out_r_testpar.wav');
-            %r_stim = conv(stim1,r_hrir);
-            %l_stim = conv(stim1,l_hrir);
-            %out = sprintf('stereo_piano_room5x5x5_walltype4_testpar_%daz.wav',head_azimuth)
-            %audiowrite(out,[l_stim r_stim], 44100)
         end
-
     end
-
 end

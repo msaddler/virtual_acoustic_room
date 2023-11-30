b07c3347bdee021073c1f3f7eb60a2924d742b04
Mark Saddler <msaddler@mit.edu>
2023-11-29 23:35:31 -0500

    generating initial spatial_audio_pipeline/assets/brir/ruggles_shinncunningham_2011 BRIRs (3 reverb levels, all at 0 elevation, 0.5s BRIRs)

--- generate_brir_dataset_job.sh
+++ generate_brir_dataset_job.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=generate_brir
 #SBATCH --out="slurm-%A_%a.out"
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=2000
+#SBATCH --mem=4000
 #SBATCH --nodes=1
 #SBATCH --time=2-00:00:00
 #SBATCH --exclude=node[017-094,097,098]
-#SBATCH --array=0-8
+#SBATCH --array=0-2
 #SBATCH --partition=normal
 
 offset=0

3f4db35a7840aa8d60402de189544b7cd5d8a22b
Mark Saddler <marksa@dtu.dk>
2025-01-18 17:14:34 -0500

    regenerating brir/v00 dataset after asymmetry bugfix

--- generate_brir_dataset.py
+++ generate_brir_dataset.py
@@ -80,10 +80,11 @@ def main(df, fn, processes=8):
 
 if __name__ == "__main__":
     index_room = int(sys.argv[1])
+    fn_manifest_brir = "/om2/scratch/Mon/msaddler/brir/v00/manifest_brir.pdpkl"
 #     fn_manifest_brir = "/om2/user/msaddler/spatial_audio_pipeline/assets/brir/v00/manifest_brir.pdpkl"
 #     fn_manifest_brir = "/om2/user/msaddler/spatial_audio_pipeline/assets/brir/mit_bldg46room1004/manifest_brir.pdpkl"
 #     fn_manifest_brir = "/om2/user/msaddler/spatial_audio_pipeline/assets/brir/eval/manifest_brir.pdpkl"
-    fn_manifest_brir = "/om2/user/msaddler/spatial_audio_pipeline/assets/brir/speech_in_noise_in_reverb/manifest_brir.pdpkl"
+    # fn_manifest_brir = "/om2/user/msaddler/spatial_audio_pipeline/assets/brir/speech_in_noise_in_reverb/manifest_brir.pdpkl"
     df_manifest_brir = pd.read_pickle(fn_manifest_brir)
     df = df_manifest_brir[df_manifest_brir.index_room == index_room]
     assert len(df) > 0, f"Found no matching BRIRs for index_room={index_room}"
--- generate_brir_dataset_job.sh
+++ generate_brir_dataset_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --job-name=generate_brir
-#SBATCH --out="slurm-%A_%a.out"
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=4000
+#SBATCH --out="trash/slurm-%A_%a.out"
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=2000
 #SBATCH --nodes=1
 #SBATCH --time=2-00:00:00
 #SBATCH --exclude=node[017-094,097,098]
-#SBATCH --array=0-4
+#SBATCH --array=0-1999
 #SBATCH --partition=normal
 
 offset=0
